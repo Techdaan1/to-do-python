0c27f62e896b880e9ce484f6706f84223658bc76
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2024-06-18 10:18:05 +0200

    Make to do list dynamic

--- home.py
+++ home.py
@@ -7,7 +7,7 @@ class Todo(object):
 
     def __str__(self):
         prio_text = "High" if self.hasPriority else "Low"
-        return "To do first: {0}, Priority: {1} ".format(self.todoDesc, prio_text)
+        return "{0}, Priority: {1} ".format(self.todoDesc, prio_text)
     
 todo_queue = deque()
 
@@ -17,21 +17,41 @@ def add_todo (todo):
     else:
         todo_queue.append(todo)
 
-def first_todo_inline():
+def first_todo():
     return todo_queue.popleft()
 
 def print_todos():
+    if not todo_queue:
+        print("There are no to-dos in the list.")
+        return
     for i,t in enumerate(todo_queue, 1):
-        print(f"{i}. {t.todoDesc}")
+        print(f"{i}. {t}")
 
 def main():
-    print("To Do List:")
-    add_todo(Todo("Clean house"))
-    add_todo(Todo("Write a letter"))
-    add_todo(Todo("Go grocery shopping", True))
-    print_todos()
-    print(first_todo_inline())
-    return
+  while True:
+        print("\nMenu:")
+        print("1. Add a to-do")
+        print("2. Show all to-dos")
+        print("3. Get the first to-do in line")
+        print("4. Exit")
+        choice = input("Choose an option (1-4): ")
 
+        if choice == '1':
+            desc = input("Enter the to-do description: ")
+            prio = input("Is it a high priority? (yes/no): ").strip().lower() == 'yes'
+            add_todo(Todo(desc, prio))
+        elif choice == '2':
+            print("To Do List:")
+            print_todos()
+        elif choice == '3':
+            if todo_queue:
+                print("First to-do:", first_todo())
+            else:
+                print("No to-dos in the list.")
+        elif choice == '4':
+            print("Exiting To Do List Manager.")
+            break
+        else:
+            print("Invalid option, please choose again.")
 if __name__ == "__main__":
     main()
